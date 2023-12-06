8a0bc52e6b5fefd798f520b566920b2023d8ef78
Zach <prettyzucchini@Zacharys-Macbook-Air.local>
2023-12-06 01:01:50 -0500

    outputs the right number

--- Module7_EC/src/main/java/com/m1/assemblyCodeTranslator.java
+++ Module7_EC/src/main/java/com/m1/assemblyCodeTranslator.java
@@ -5,19 +5,19 @@ import java.util.Map;
 
 public class assemblyCodeTranslator {
 
-// Define the opcode values for MOV instruction
-    private static final String OPCODE_MOV = "0100";
+    // Define the opcode values for MOV instruction
+    private static final String OPCODE_MOV = "01";
 
     // Define the register mappings
     private static final Map<String, String> registerMap = new HashMap<>();
     static {
-        registerMap.put("R0", "0000");
-        registerMap.put("R1", "0001");
-        registerMap.put("R2", "0010");
+        registerMap.put("R0", "00");
+        registerMap.put("R1", "01");
+        registerMap.put("R2", "10");
         // Add mappings for other registers
         for (int i = 0; i <= 15; i++) {
             String registerName = "R" + i;
-            registerMap.put(registerName, String.format("%04d", Integer.parseInt(Integer.toBinaryString(i))));
+            registerMap.put(registerName, String.format("%02d", Integer.parseInt(Integer.toBinaryString(i))));
         }
     }
 
@@ -33,9 +33,11 @@ public class assemblyCodeTranslator {
 
         // Lookup register codes
         String destRegisterCode = registerMap.get(destRegister);
-        String sourceRegisterCode = registerMap.get(sourceRegister);
-
-        // Form the machine code
-        return "e1a" + destRegisterCode + sourceRegisterCode + "000000000000";
+        String sourceRegisterCode = Integer.toString(Integer.parseInt(registerMap.get(sourceRegister), 2));
+        System.out.println("destRegisterCode " + destRegisterCode);
+        System.out.println("sourceRegisterCode " + sourceRegisterCode);
+        // Form the machine code with the desired format
+        return "e1a" + destRegisterCode + "00" + sourceRegisterCode;
     }
+
 }
