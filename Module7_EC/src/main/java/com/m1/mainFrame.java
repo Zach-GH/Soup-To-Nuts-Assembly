a931ce2449503f20e098cb7ec7ab951fe03f9935
Zach <prettyzucchini@Zacharys-Macbook-Air.local>
2023-12-09 02:23:43 -0500

    fully working

--- Module7_EC/src/main/java/com/m1/mainFrame.java
+++ Module7_EC/src/main/java/com/m1/mainFrame.java
@@ -13,7 +13,7 @@ public class mainFrame extends JFrame implements ActionListener {
     static JButton assemblerBtn, disassemblerBtn, clearBtn;
     static JFrame frame;
     static JLabel assemblerLabel, disassemblerLabel;
-    static JTextField aTextField1, aTextField2, aTextField3, dTextField;
+    static JTextField aTextField1, aTextField2, aTextField3, aTextField4, dTextField;
 
     mainFrame() {
         setTitle("M7 Extra Credit Zachary Meisner");
@@ -25,29 +25,26 @@ public class mainFrame extends JFrame implements ActionListener {
         assemblerLabel = createLabel("0");
         disassemblerLabel = createLabel("0");
 
-        aTextField1 = createTextField(15, 165, 20);
-        aTextField2 = createTextField(15, 285, 20);
-        aTextField3 = createTextField(15, 405, 20);
-        dTextField = createTextField(15, 165, 130);
+        aTextField1 = createTextField(15, 125, 20);
+        aTextField2 = createTextField(15, 245, 20);
+        aTextField3 = createTextField(15, 365, 20);
+        aTextField4 = createTextField(15, 485, 20);
+        dTextField = createTextField(15, 125, 130);
 
         Font font = new Font("Comic Sans", Font.BOLD, 20);
-        setFontForTextFields(font, aTextField1, aTextField2, aTextField3, dTextField);
+        setFontForTextFields(font, aTextField1, aTextField2, aTextField3, aTextField4, dTextField);
 
-        JLabel commaLabel = createLabel(",");
-        commaLabel.setFont(font);
-
-        commaLabel.setBounds(400, 20, 20, 80);
-        assemblerBtn.setBounds(165, 70, 115, 55);
-        dTextField.setBounds(165, 130, 115, 55);
-        disassemblerBtn.setBounds(165, 180, 115, 55);
-        clearBtn.setBounds(165, 320, 115, 55);
+        assemblerBtn.setBounds(125, 70, 115, 55);
+        dTextField.setBounds(125, 130, 115, 55);
+        disassemblerBtn.setBounds(125, 180, 115, 55);
+        clearBtn.setBounds(125, 320, 115, 55);
         assemblerLabel.setBounds(300, 70, 299, 55);
         disassemblerLabel.setBounds(300, 180, 299, 55);
 
         add(aTextField1);
         add(aTextField2);
-        add(commaLabel);
         add(aTextField3);
+        add(aTextField4);
         add(assemblerBtn);
         add(dTextField);
         add(disassemblerBtn);
@@ -62,7 +59,7 @@ public class mainFrame extends JFrame implements ActionListener {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
         pack();
-        setSize(600, 500);
+        setSize(700, 500);
         setVisible(true);
     }
 
@@ -101,6 +98,7 @@ public class mainFrame extends JFrame implements ActionListener {
         String aTmpText1;
         String aTmpText2;
         String aTmpText3;
+        String aTmpText4;
         String dTmpText;
 
         if (s.equals("Assemble")) {
@@ -108,11 +106,12 @@ public class mainFrame extends JFrame implements ActionListener {
            aTmpText1 = aTextField1.getText().toString();
            aTmpText2 = aTextField2.getText().toString();
            aTmpText3 = aTextField3.getText().toString();
+           aTmpText4 = aTextField4.getText().toString();
 
             // Translate machine code to assembly
            if ((!(aTextField1.getText().equals(""))) && (!(aTextField2.getText().equals(""))) && (!(aTextField3.getText().equals("")))) {
               // mov r1 r2
-              String machineCode = act.translateMOV(aTmpText1, aTmpText2, aTmpText3);
+              String machineCode = act.translateASM(aTmpText1, aTmpText2, aTmpText3, aTmpText4);
               assemblerLabel.setText("<html>" + machineCode + "</html>");
             } else {
               assemblerLabel.setText("<html>" + "Please enter a valid value" + "</html>");
@@ -136,6 +135,7 @@ public class mainFrame extends JFrame implements ActionListener {
             aTextField1.setText("");
             aTextField2.setText("");
             aTextField3.setText("");
+            aTextField4.setText("");
             dTextField.setText("");
         }
     }
